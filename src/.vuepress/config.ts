2c671fe99569269b4ac13aa8e23b36b782ca30d9
weijordan <12012972+weijordan@user.noreply.gitee.com>
2024-10-15 09:55:41 +0800

    add oml2dPlugin

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,6 +1,8 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
+import { oml2dPlugin } from 'vuepress-plugin-oh-my-live2d';
+
 export default defineUserConfig({
 
   base: "/blog/",
@@ -24,9 +26,34 @@ export default defineUserConfig({
   // },
 
   theme,
- // 和 PWA 一起启用
+  // 和 PWA 一起启用
   // shouldPrefetch: false,
-  
-  
+  plugins: [
+    // 看板娘插件
+    oml2dPlugin({
+      // 在这里配置选项
+      models: [
+        {
+          path: 'https://cdn.jsdelivr.net/gh/Eikanya/Live2d-model/Live2D/Senko_Normals/senko.model3.json',
+          scale: 0.12,
+          position: [-10, 50],
+          stageStyle: {
+            width: 350
+          }
+        }
+        // {
+        //   path: "https://model.oml2d.com/Pio/model.json",
+        //   scale: 0.4,
+        //   position: [0, 50],
+        //   stageStyle: {
+        //     "height": 300
+        //   }
+        // }
+      ]
+    })
+
+    //  ...other plugins
+  ]
+
 });
 
